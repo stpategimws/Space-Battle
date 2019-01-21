6494627c53a73d2b5247b9b403df041d66043e29
stpategimws <46881623+stpategimws@users.noreply.github.com>
2019-01-21 12:20:14 +0700

    Add files via upload

---
+++ MusicSettings.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicSettings : MonoBehaviour
+{
+    public Slider slider;
+    public static float volume=1;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //Сохраненные настройки при старте игры
+        //Звук
+        volume = PlayerPrefs.GetFloat("volume", volume);
+        slider.value = volume;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        AudioListener.volume = volume;
+        //Сохранение звука
+        volume = slider.value;
+        PlayerPrefs.SetFloat("volume", volume);
+    }
+}
