629bf12b1b4328013c81431133d9afdd4857114b
stpategimws <46881623+stpategimws@users.noreply.github.com>
2019-01-21 12:18:18 +0700

    Add files via upload

---
+++ EnemyController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform EnemySpawn;
+    public GameObject Spawn;
+    public GameObject Explosion;
+    public GameObject BulletEnemy;
+    public GameObject medical;
+    public GameObject Invulnerability;
+    public GameObject Shield;
+    public static int CurrentBulletIndex = 0;
+    public static int CurrentBulletIndex_2 = 0;
+    public float timer = 2f;
+    public int Speed;
+
+    //Эксперимент
+    public Transform MedicalSpawn;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Explosion.transform.position = gameObject.transform.position;
+            Explosion.SetActive(true);
+            gameObject.SetActive(false);
+            gameObject.transform.position = Spawn.transform.position;
+            if (InvulnerabilityController.Invul==false)
+            {
+                HealthController.Health = HealthController.Health - (10 + ComplicationController.ComplicationScore);
+            }
+        }
+        if (collision.tag == "Bullet")
+        {
+            Explosion.transform.position = gameObject.transform.position;
+            Explosion.SetActive(true);
+            gameObject.SetActive(false);
+            //Explosion.SetActive(true);
+            gameObject.transform.position = Spawn.transform.position;
+            ScoreController.Score++;
+        }
+
+    }
+
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (gameObject.activeSelf==false)
+        {
+            BulletEnemy.SetActive(false);
+        }
+
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            Rigidbody2D bulletRB = EnemySpawn.GetChild(CurrentBulletIndex).GetComponent<Rigidbody2D>();
+            bulletRB.gameObject.SetActive(true);
+            bulletRB.position = Spawn.transform.position - Vector3.left;
+            bulletRB.velocity = Vector2.zero;
+            bulletRB.AddForce(Vector2.left * Speed, ForceMode2D.Impulse);
+            CurrentBulletIndex++;
+            if (CurrentBulletIndex >= EnemySpawn.childCount)
+            {
+                CurrentBulletIndex = 0;
+                timer = 2f;
+            }
+        }
+    }
+}
