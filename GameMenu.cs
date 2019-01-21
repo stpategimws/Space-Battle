87777ff092860acf5968d6ecf7da3f98a8d83d56
stpategimws <46881623+stpategimws@users.noreply.github.com>
2019-01-21 12:19:43 +0700

    Add files via upload

---
+++ GameMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour
+{
+    public GameObject gameOver;
+    public GameObject Panel;
+    public bool paused;
+
+    public void OpenPanel()
+    {
+        Panel.SetActive(true);
+        Time.timeScale = 0;
+        paused = true;
+    }
+
+    public void ClosedPanel()
+    {
+        Panel.SetActive(false);
+        Time.timeScale = 1;
+        paused = false;
+    }
+
+    public void NewGame()
+    {
+        gameOver.SetActive(false);
+        Time.timeScale = 1;
+        paused = false;
+        AdvertisingController.Counter = 1;
+        ScoreController.Score = 0;
+        HealthController.Health = 100;
+        ComplicationController.ComplicationScore = 0;
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitMainMenu()
+    {
+        Time.timeScale = 1;
+        paused = false;
+        AdvertisingController.Counter = 1;
+        SceneManager.LoadScene(0);
+    }
+	
+    // Use this for initialization
+	void Start ()
+    {
+        gameOver.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
