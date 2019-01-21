7e9945f5810b2173a331b5a6cb305e276a399c19
stpategimws <46881623+stpategimws@users.noreply.github.com>
2019-01-21 12:22:25 +0700

    Add files via upload

---
+++ BulletController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public GameObject BulletSpawn;
+    public Transform bulletPoolTr;
+    private int CurrentBulletIndex = 0;
+
+    public void ButtonFire()
+    {
+        GetComponent<AudioSource>().Play();
+        GetComponent<AudioSource>().volume = 0.3f;
+        Rigidbody2D bulletRB = bulletPoolTr.GetChild(CurrentBulletIndex).GetComponent<Rigidbody2D>();
+        bulletRB.gameObject.SetActive(true);
+        bulletRB.position = BulletSpawn.transform.position + Vector3.right;
+        bulletRB.velocity = Vector2.zero;
+        bulletRB.AddForce(Vector2.right * 25, ForceMode2D.Impulse);
+        CurrentBulletIndex++;
+        if (CurrentBulletIndex >= bulletPoolTr.childCount)
+        {
+            CurrentBulletIndex = 0;
+        }
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
