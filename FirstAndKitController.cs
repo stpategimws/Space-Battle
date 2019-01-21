ed92b31a7985a2e8edd8f27bc7ebabdf538598e4
stpategimws <46881623+stpategimws@users.noreply.github.com>
2019-01-21 12:21:02 +0700

    Add files via upload

---
+++ FirstAndKitController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstAndKitController : MonoBehaviour
+{
+    public GameObject Spawn;
+    Rigidbody2D rb;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if(HealthController.Health!=100)
+            {
+                HealthController.Health += 10;
+                gameObject.SetActive(false);
+                gameObject.transform.position = Spawn.transform.position;
+            }
+        }
+    }
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();	
+	}
+
+    // Update is called once per frame
+    void Update ()
+    {
+	    	
+	}
+}
