10945d2e40563e467bdb767a9f9cd9fae60ebcde
stpategimws <46881623+stpategimws@users.noreply.github.com>
2019-01-21 12:20:48 +0700

    Add files via upload

---
+++ ComplicationController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComplicationController : MonoBehaviour
+{
+    public static int ComplicationScore = 0;
+    private float timer = 0;
+	
+    // Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timer += Time.deltaTime;
+        if (timer>=20)
+        {
+            ++ComplicationScore;
+            timer = 0;
+            return;
+        }
+	}
+}
