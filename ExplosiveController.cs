17a24c543fc214f718036f6036b15d6a1239058f
stpategimws <46881623+stpategimws@users.noreply.github.com>
2019-01-21 12:18:52 +0700

    Add files via upload

---
+++ ExplosiveController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveController : MonoBehaviour
+{
+    private float timer = 0.5f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (gameObject.activeSelf)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                gameObject.SetActive(false);
+                timer = 0.5f;
+            }
+        }
+	}
+}
