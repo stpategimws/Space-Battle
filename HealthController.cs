e57d77a2da84179779693951fa4aba818e417af8
stpategimws <46881623+stpategimws@users.noreply.github.com>
2019-01-21 12:21:20 +0700

    Add files via upload

---
+++ HealthController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour
+{
+    public Text textHealth;
+    public static int Health = 100;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        textHealth.text = "Health: " + Health;
+        if (Health > 100)
+            Health = 100;
+        if (Health < 0)
+            Health = 0;
+	}
+}
