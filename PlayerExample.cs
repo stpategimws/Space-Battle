25bb228dd8d2db46d3e3a69079280bf86ac492b0
stpategimws <46881623+stpategimws@users.noreply.github.com>
2019-01-21 12:22:52 +0700

    Add files via upload

---
+++ PlayerExample.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PlayerExample : MonoBehaviour {
+
+    private float moveSpeed=3;
+    public Joystick joystick;
+
+    void Start()
+    {
+          
+    }
+
+    void Update () 
+	{
+        if (FixedJoystick.T==false)
+        {
+            moveSpeed = 3;
+        }
+        else if (FixedJoystick.T==true)
+        {
+            if(moveSpeed<=15)
+                moveSpeed += 0.05f;
+        }
+
+        Vector2 moveVector = (transform.right * joystick.Horizontal + transform.forward * joystick.Vertical).normalized;
+        transform.Translate(moveVector * moveSpeed * Time.deltaTime);
+
+        Vector2 moveVector2 = (transform.up * joystick.Vertical + transform.forward * joystick.Horizontal).normalized;
+        transform.Translate(moveVector2 * moveSpeed * Time.deltaTime);
+
+        /*
+        Vector3 moveVector = (transform.right * joystick.Horizontal + transform.forward * joystick.Vertical).normalized;
+        transform.Translate(moveVector * moveSpeed * Time.deltaTime);
+
+        Vector3 moveVector2 = (transform.up * joystick.Vertical + transform.forward * joystick.Horizontal).normalized;
+        transform.Translate(moveVector2 * moveSpeed * Time.deltaTime);
+        */
+
+
+    }
+}
