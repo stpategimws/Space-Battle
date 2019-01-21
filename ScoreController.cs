45b9d0f99999931fc6f86d5ed5b319ede1c39153
stpategimws <46881623+stpategimws@users.noreply.github.com>
2019-01-21 12:21:52 +0700

    Add files via upload

---
+++ ScoreController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    public Text textScore;
+    public static int Score = 0;
+    public static int record;
+
+    // Use this for initialization
+    void Start ()
+    {
+        Score = 0;
+        textScore.text = "Score: " + Score;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        textScore.text = "Score: " + Score;
+
+        //Сохранение рекорда
+        if (Score > record)
+        {
+            record = Score;
+            PlayerPrefs.SetInt("savescore", record);
+            PlayerPrefs.Save();
+        }
+        record = PlayerPrefs.GetInt("savescore");
+    }
+}
