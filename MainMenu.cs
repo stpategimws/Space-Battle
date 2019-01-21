77d14a2be40b7a0f41bc71111d22c8208f4cba1e
stpategimws <46881623+stpategimws@users.noreply.github.com>
2019-01-21 12:20:00 +0700

    Add files via upload

---
+++ MainMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject PanelSetting;
+	
+    public void StartGames()
+    {
+        HealthController.Health = 100;
+        ComplicationController.ComplicationScore = 0;
+        SceneManager.LoadScene(1);
+    }
+
+    public void OpenPanelSetting()
+    {
+        PanelSetting.SetActive(true);
+    }
+
+    public void ClosedPanelSetting()
+    {
+        PanelSetting.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
