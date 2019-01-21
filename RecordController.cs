8ae23bcaee47803048eaa941d3a36120a6467d16
stpategimws <46881623+stpategimws@users.noreply.github.com>
2019-01-21 12:20:29 +0700

    Add files via upload

---
+++ RecordController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecordController : MonoBehaviour
+{
+    public Text text;
+
+    // Use this for initialization
+    void Start ()
+    {
+        text.text = "Record: " + PlayerPrefs.GetInt("savescore").ToString();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
